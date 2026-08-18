be0de09f56c8ad89658be1349386bf4c0e250dad
RyoLeee <h4285007@gmail.com>
2026-08-18 22:01:56 +0700

    Create system plan

--- src/modules/automation-engine/filtering-engine/filtering-engine/cleanup-engine.ts
+++ src/modules/automation-engine/filtering-engine/filtering-engine/cleanup-engine.ts
@@ -5,3 +5,6 @@ const DELETE_LIMIT = 10;
 export async function cleanupStaleExtensions() {
   return await deleteStaleExtensions(DELETE_LIMIT);
 }
+
+
+// need to be improved in return area so loging can give better informtation
---
+++ src/modules/twentyFourHour/automation.md
@@ -0,0 +1,4 @@
+Createing middleware - is users premium?
+
+every 1H - GetEkstension + Cleanup 
+every 24H - Export extension to UIUX + clean ---> drop to tabel
