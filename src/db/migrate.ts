3a2aee8102f119bef5b9e9b9ecb6f45db6462db1
RyoLeee <h4285007@gmail.com>
2026-08-29 19:55:37 +0700

    chore: clean up database migrations

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -3,10 +3,9 @@ import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { Pool } from "pg";
 import * as dotenv from "dotenv";
 import path from "node:path";
+import { config } from "../../config"
 
-dotenv.config();
-
-const url = process.env.DATABASE_URL;
+const url = config.dbUrl;
 
 if (!url) {
   throw new Error("DATABASE_URL is not defined");
